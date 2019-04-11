a38004d85f5bc112e86f343e680b8281b7a90e2d
Dan VerWeire <dverweire@gmail.com>
2019-04-11 13:35:14 -0400

    modify tests to use outstream instead of reading file from disk based on filename
    the filenames change depending on stat.mtime, so tests incorrectly
    fail if you clone the repository

--- test/test.js
+++ test/test.js
@@ -2,51 +2,52 @@ const rs = require('../')({ cache : __dirname + '/cache' });
 const test = require('tape');
 const fs = require('fs');
 const crypto = require('crypto');
+const through = require('through');
 
 test("Testing an image", function(t) {
+	t.plan(2);
 
 	//Variables for the file name and expect sha hash.
-	const fileName = "test/cache/77141-Model-500x500-1554300855285-100x100.jpg";
 	const expectedHash = "ef18a76f7917a9a7fd0e730147c5300cf4da4646";
+	const outstream = passthrough();
+
+	//Call test equality with the files and the tape object.
+	testEquality(outstream, expectedHash, t);
 
 	//Call maybeRender with the correct options.
-	rs.maybeRender({
+	rs({
 		path : "./test/77141-Model-500x500.jpg"
 		, width : 100
 		, height : 100
 		, format : 'jpg'
+		, outstream : outstream
 	}, function (err) {
-
 		//Make sure there isn't an error.
 		t.notOk(err, "There should not be an error");
-
-		//Call test equality with the files and the tape object.
-		testEquality(fileName, expectedHash, t);
-
-		//End the test.
 		t.end();
 	});
 });
 
 
 test("Testing a video", function(t) {
+	t.plan(2);
 
 	//Variables for the file name and expect sha hash.
-	const fileName = "test/cache/10043-Video-1554302883754-bitrate:500.mp4";
-	const expectedHash = "1ad1e9b0354c6b2e862393bc99ab8b1cc429a271";
+	const expectedHash = "f66204516e34cc27ef2b114e45a24578ebb071e6";
+	const outstream = passthrough();
+
+	//Call test equality with the files and the tape object.
+	testEquality(outstream, expectedHash, t);
 
 	//Call maybeRender with the correct options.
-	rs.maybeRender({
+	rs({
 		path : "./test/10043-Video.mp4"
 		, format: "mp4"
 		, bitrate: 500
+		, outstream : outstream
 	}, function (err) {
-
 		//Make sure there isn't an error.
 		t.notOk(err, "There should not be an error");
-
-		//Call test equality with the files and the tape object.
-		testEquality(fileName, expectedHash, t);
 
 		//End the test.
 		t.end();
@@ -54,74 +55,74 @@ test("Testing a video", function(t) {
 });
 
 test("Testing getting a screenshot and timestamp", function(t) {
+	t.plan(2);
 
 	//Variables for the file name and expect sha hash.
-	const fileName = "test/cache/10043-Video-1554302883754-size:320x240-timestamp:17.jpg";
-	const expectedHash = "990450fc686532c26963bf7251924f78dbb08128"
+	const expectedHash = "990450fc686532c26963bf7251924f78dbb08128";
+	const outstream = passthrough();
 
-	rs.maybeRender({
+	//Call test equality with the files and the tape object.
+	testEquality(outstream, expectedHash, t);
+
+	rs({
 		path : "./test/10043-Video.mp4"
 		, timestamp : '17'
 		, size: "320x240"
 		, format: "jpg"
+		, outstream : outstream
 	}, function (err) {
-		
 		//Make sure there isn't an error.
 		t.notOk(err, "There should not be an error");
-
-		//Call test equality with the files and the tape object.
-		testEquality(fileName, expectedHash, t);
 		
 		//End the test.
 		t.end();
-
 	});
 });
 
-
 test("Testing converting a video to avi", function(t) {
+	t.plan(2);
 
 	//Variables for the file name and expect sha hash.
-	const fileName = "test/cache/10043-Video-1554302883754-bitrate:2000.avi";
-	const expectedHash = "31d09922742b3350f907bc5fd6113bb45dc0c9c5"
+	const expectedHash = "fa75abacb6e97296f4753c4c0f295e8d3a66d632";
+	const outstream = passthrough();
 
-	rs.maybeRender({
+	//Call test equality with the files and the tape object.
+	testEquality(outstream, expectedHash, t);
+
+	rs({
 		path : "./test/10043-Video.mp4"
 		, format: "avi"
 		, bitrate: 2000
+		, outstream : outstream
 	}, function (err) {
-		
 		//Make sure there isn't an error.
 		t.notOk(err, "There should not be an error");
 
-		//Call test equality with the files and the tape object.
-		testEquality(fileName, expectedHash, t);
-		
 		//End the test.
 		t.end();
-
 	});
 });
 
 test("Testing a video with framerate", function(t) {
+	t.plan(2);
 
 	//Variables for the file name and expect sha hash.
-	const fileName = "test/cache/10043-Video-1554302883754-bitrate:500-framerate:5.mp4";
-	const expectedHash = "77b41b55b28a2ae48a03a75578cd68765f751f15"
+	const expectedHash = "6a9486bdf2410238a59757429fc31bec8690baab";
+	const outstream = passthrough();
 
-	rs.maybeRender({
+	//Call test equality with the files and the tape object.
+	testEquality(outstream, expectedHash, t);
+
+	rs({
 		path : "./test/10043-Video.mp4"
 		, format: "mp4"
 		, bitrate: 500
 		, framerate: 5
+		, outstream : outstream
 	}, function (err) {
-		
 		//Make sure there isn't an error.
 		t.notOk(err, "There should not be an error");
 
-		//Call test equality with the files and the tape object.
-		testEquality(fileName, expectedHash, t);
-		
 		//End the test.
 		t.end();
 
@@ -129,28 +130,28 @@ test("Testing a video with framerate", function(t) {
 });
 
 test("Testing a video with aspectratio", function(t) {
+	t.plan(2);
 
 	//Variables for the file name and expect sha hash.
-	const fileName = "test/cache/10043-Video-1554302883754-height:200-aspect_ratio:8x5-bitrate:400.mp4";
-	const expectedHash = "6c3a55f0bc0ded59e1faf6a663ce692a997e5b88"
+	const expectedHash = "6c3a55f0bc0ded59e1faf6a663ce692a997e5b88";
+	const outstream = passthrough();
 
-	rs.maybeRender({
+	//Call test equality with the files and the tape object.
+	testEquality(outstream, expectedHash, t);
+
+	rs({
 		path : "./test/10043-Video.mp4"
 		, format: "mp4"
 		, bitrate: 400
 		, height : 200
 		, aspect_ratio: '8x5'
+		, outstream : outstream
 	}, function (err) {
-		
 		//Make sure there isn't an error.
 		t.notOk(err, "There should not be an error");
-
-		//Call test equality with the files and the tape object.
-		testEquality(fileName, expectedHash, t);
 		
 		//End the test.
 		t.end();
-
 	});
 });
 
@@ -161,7 +162,7 @@ test("Testing a video with aspectratio", function(t) {
  * @param {*} file2 name of file2
  * @param {*} t tape testing object.
  */
-function testEquality (file1, expectedHash, t) {
+function testEquality (stream, expectedHash, t) {
 
 	try {
 
@@ -171,21 +172,26 @@ function testEquality (file1, expectedHash, t) {
 		//Create the shasum;
 		let shasum = crypto.createHash(algorithm);
 
-		//Read in the file.
-		let file = fs.ReadStream(file1);
-
 		//On data, update the hash.
-		file.on('data', resultHash => {
+		stream.on('data', resultHash => {
 			shasum.update(resultHash);
 		});
 
 		//On end, convert the hash into hex and compare the hashes are equal.
-		file.on('end', () => {
+		stream.on('end', () => {
 			let resultHash = shasum.digest('hex');
-			t.equal(expectedHash, resultHash, "Hashes are equal");
+			t.equal(resultHash, expectedHash, "Hashes are equal");
 		});
 
 	} catch(e) {
 		console.log("Error:", e.stack);
 	}
+}
+
+function passthrough() {
+	return through(function write(data) {
+		this.queue(data);
+	}, function end () {
+		this.queue(null)
+	})
 }
